9e1a074eb913d563212b12bce20c6e95cc7effd2
HJ <164671672+HJayBTDH27@users.noreply.github.com>
2024-11-12 17:39:57 -0500

    Initial commit

---
+++ lab06/dice_roller.js
@@ -0,0 +1,16 @@
+let diceValues = [0,0,0,0,0];
+
+function rollDice() {
+    let x = Math.floor(Math.random() * 6) + 1;
+    return x;
+}
+
+function rollFiveDice() {
+
+    for (let i = 0; i < 5; i++) {
+
+        diceValues[i] = rollDice();
+
+    }
+
+}
--- lab06/yatzy_dice_server.js
+++ lab06/yatzy_dice_server.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path'); 
 
+app.use(express.static('public'));
+
+app.get('/roll-dices', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'lab06_yatzy_tester.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
